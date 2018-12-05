ba05767d39bd25f150e28a899bc9298c20388817
Ryan Abraham <rabraham@uaudio.com>
2018-12-04 16:52:17 -0800

    Remove server.js as it's not needed to run the compiled app

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-app.use(express.static('public'));
-
-app.listen(3000, () => {
-  console.log('Express server is up on port 3000');
-});
